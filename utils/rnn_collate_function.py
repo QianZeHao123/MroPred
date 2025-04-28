51e5090b795248315cb8ff331b2a062870679460
Zehao Qian <zehao.qian.cn@gmail.com>
2025-04-28 13:23:33 +0800

    add collate_fn for rnn data

---
+++ utils/rnn_collate_function.py
@@ -0,0 +1,23 @@
+import torch
+from torch.nn.utils.rnn import pad_sequence
+
+
+def collate_fn(batch):
+    """
+    use collate_fn to pad sequences to the same length within a batch
+    """
+    # batch is a list of (sequence, target) pairs
+    sequences, targets = zip(*batch)
+
+    # use pad_sequence to pad sequences to the same length
+    padded_sequences = pad_sequence(
+        sequences, batch_first=True, padding_value=0.0, padding_side="left"
+    )
+    padded_targets = pad_sequence(
+        targets, batch_first=True, padding_value=0.0, padding_side="left"
+    )
+    lengths = torch.tensor(
+        [len(seq) for seq in sequences]
+        # [seq.shape[0] for seq in sequences]
+    )  # get the original length of each sequence
+    return padded_sequences, padded_targets, lengths
