08f823a18a8fa0f2fd14056b9b0fcee95bb139fa
Zehao Qian <zehao.qian.cn@gmail.com>
2025-05-30 18:17:18 +0800

    add a function of data preprocessing

--- model/__init__.py
+++ model/__init__.py
@@ -1,6 +1,6 @@
 from .loss_fun import FocalLoss
 from .model import RnnModel
 from .dataset import mroRnnDataset
+from .data_prep import preprocess_data
 
-
-__all__ = [FocalLoss, RnnModel, mroRnnDataset]
+__all__ = [FocalLoss, RnnModel, mroRnnDataset, preprocess_data]
