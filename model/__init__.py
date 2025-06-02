097f5e79d7769c1d30bd26c67eaac925d7556e13
Zehao Qian <zehao.qian.cn@gmail.com>
2025-06-02 23:00:54 +0800

    add model eval to module init

--- model/__init__.py
+++ model/__init__.py
@@ -2,5 +2,7 @@ from .loss_fun import FocalLoss
 from .model import RnnModel
 from .dataset import mroRnnDataset
 from .data_prep import preprocess_data
+from .eval import model_eval
 
-__all__ = [FocalLoss, RnnModel, mroRnnDataset, preprocess_data]
+
+__all__ = [FocalLoss, RnnModel, mroRnnDataset, preprocess_data, model_eval]
