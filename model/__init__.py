40ce2119708015f8e9a7b0e1b657302f4c66619e
Zehao Qian <zehao.qian.cn@gmail.com>
2025-06-03 19:35:30 +0800

    add no_day_mileage data preprocessing function

--- model/__init__.py
+++ model/__init__.py
@@ -2,7 +2,15 @@ from .loss_fun import FocalLoss
 from .model import RnnModel
 from .dataset import mroRnnDataset
 from .data_prep import preprocess_data
+from .data_prep_nomil import preprocess_data_nomil
 from .eval import model_eval
 
 
-__all__ = [FocalLoss, RnnModel, mroRnnDataset, preprocess_data, model_eval]
+__all__ = [
+    FocalLoss,
+    RnnModel,
+    mroRnnDataset,
+    preprocess_data,
+    preprocess_data_nomil,
+    model_eval,
+]
